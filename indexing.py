bafaea960857bd3368c6d79969ee49e3f7567774
vedsb <143119170+vedsb@users.noreply.github.com>
2025-02-26 16:48:48 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,10 @@
+# [start:end:step]
+
+credit_no = "1234-5678-9012-3456"
+
+#print(credit_no[0])
+#print(credit_no[:4])
+print(credit_no[5:9])
+print(credit_no[5:])
+print(credit_no[-1])
+print(credit_no[::2])
