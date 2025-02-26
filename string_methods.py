68af936015938b13cb3235614d14591dcde67946
vedsb <143119170+vedsb@users.noreply.github.com>
2025-02-26 15:55:14 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,22 @@
+#name = input("enter your full name")
+#result = len(name)
+phnno = input("enter ur phn no")
+
+#result = name.find("o")
+#result = name.rfind("o")
+#result = name.isdigit()
+#result = name.isalpha()
+
+#name = name.capitalize()
+#name = name.upper()
+#name = name.lower()
+
+#result = phnno.count("-")
+phnno = phnno.replace("-", "")
+
+
+
+
+print(phnno)
+#print(result)
+#print(name)
